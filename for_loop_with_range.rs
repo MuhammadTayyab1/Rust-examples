83f5a7e1ccdb1de0d587c1705fffe074bfafb741
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-03-03 10:54:47 -0800

    For loop with range

---
+++ for_loop_with_range.rs
@@ -0,0 +1,12 @@
+
+use std::io;
+
+
+fn main() { 
+               for i in 1..10 
+               {
+                   println!("{}", i);
+                   println!("For loop with range");
+               }
+
+}
