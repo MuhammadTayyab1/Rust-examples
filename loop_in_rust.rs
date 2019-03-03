34039c94018ec1877e243f640fd371ec40039411
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-03-02 20:38:18 -0800

    loop in Rust

---
+++ loop_in_rust.rs
@@ -0,0 +1,22 @@
+
+use std::io;
+
+
+fn main() { 
+
+             loop 
+                
+                {
+            
+                println!("guess number");
+                println!("enter your guess number");
+                let mut guess = String::new();
+                io::stdin().read_line(&mut guess)
+                .expect("failed");
+                println!("guess    {} : ",guess);
+
+                
+
+               }	     
+                  
+}
