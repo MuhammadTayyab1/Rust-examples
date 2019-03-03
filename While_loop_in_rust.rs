20ab1f4de2f53278b958960be5f9d64a21307da2
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-03-03 10:54:15 -0800

    While_loop

---
+++ While_loop_in_rust.rs
@@ -0,0 +1,12 @@
+
+use std::io;
+
+
+fn main() { 
+             let mut i: u8 = 1;
+            while i <= 100 
+           {
+                 println!("{}", i);
+                 i += 5;
+           }
+}
