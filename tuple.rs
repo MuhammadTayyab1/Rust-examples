a24e74b778c841760db44380c296ffd47a68a34e
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-03-16 23:55:31 +0500

    Tuples

---
+++ tuple.rs
@@ -0,0 +1,12 @@
+fn main()
+{
+    let x=(2,1.5,"text",'A',"@#$%^&");
+   
+    println!("integer     = {0}   ",x.0);
+    println!("float       = {0}     ",x.1);
+    println!("string      = {0}    ",x.2);
+    println!("char        = {0}      ",x.3);
+    println!("string      = {0}    ",x.4);
+    
+    
+}
