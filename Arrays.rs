136aabe48e80c2200281c47468a5b7db37388085
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-03-17 04:42:10 -0700

    Arrays

---
+++ Arrays.rs
@@ -0,0 +1,29 @@
+
+fn main() {
+        
+         println!("Array of numbers");
+         println!("=========================================");
+         let mut numdata=[100,90,87,88,95,95,99,100,150,100,92];
+ 
+         for i in 0..9
+         {
+
+            println!("{0}",numdata[i]);
+
+         }
+         println!("=========================================");
+         println!(" ");
+         println!("Array of text");
+         println!("=========================================");
+         let mut textdata=["text","my text","own data","rust","webassembly","country","computer","cargo run","function","start"];
+
+         for j in 0..textdata.len()
+         {
+
+            println!("{0}",textdata[j]);
+         }
+         println!("=========================================");
+       
+
+         
+}
