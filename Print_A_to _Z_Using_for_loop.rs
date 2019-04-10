112c82a409e92e0168472dfbff2a8710d60a95e8
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-04-10 12:43:22 -0700

    Update Print_A_to _Z_Using_for_loop.rs

--- Print_A_to _Z_Using_for_loop.rs
+++ Print_A_to _Z_Using_for_loop.rs
@@ -7,14 +7,15 @@ fn main() {
     let mut count1=97;
     for i in 0..26
     { 
-
+     
     let cap = [count];
     let sam = [count1];
    
-
+   
     let capital = String::from_utf8_lossy(&cap);
     let small = String::from_utf8_lossy(&sam);
 
+        // print 
     println!("{0} {1}", capital,small);
     count+=1;
     count1+=1;
