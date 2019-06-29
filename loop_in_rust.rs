b6bb11b75dd0b9f408eded1155333fc51785c9d6
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-06-29 12:06:16 -0700

    Update loop_in_rust.rs

--- loop_in_rust.rs
+++ loop_in_rust.rs
@@ -5,18 +5,13 @@ use std::io;
 fn main() { 
 
              loop 
-                
                 {
-            
                 println!("guess number");
                 println!("enter your guess number");
                 let mut guess = String::new();
                 io::stdin().read_line(&mut guess)
                 .expect("failed");
                 println!("guess    {} : ",guess);
-
-                
-
                }	     
                   
 }
